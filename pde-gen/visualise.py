7cba6ad76acef9d447883c71b4271b27748f3827
MBUYt0n <shusrith03@gmail.com>
2024-11-11 20:26:41 +0530

    pde gen i swear

---
+++ pde-gen/visualise.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+from tqdm import tqdm
+
+
+def visualize_burgers(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter of the data shard to be visualized
+    """
+
+    xcrd = np.load("data/x_coordinate.npy")
+    data = np.load(path)
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+
+    for i in tqdm(range(data.shape[0])):
+        if i == 0:
+            im = ax.plot(xcrd, data[i].squeeze(), animated=True, color="blue")
+        else:
+            im = ax.plot(
+                xcrd, data[i].squeeze(), animated=True, color="blue"
+            )  # show an initial one first
+        ims.append([im[0]])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("gifs/burgerNoisy.gif", writer=writer)
+
+visualize_burgers("data/burgerNoisy.npy")
